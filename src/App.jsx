57a1d9706c65df9576ede9ddcafbfc81a1a87b8a
Igor Gabriel <igorgabrielr.andrade@gmail.com>
2024-06-10 00:36:46 -0300

    section 1 finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BsPhone } from "react-icons/bs";
+
 
 function App() {
   return (
     <>
-      {/* header begining */}
+      {/* header start */}
       <header>
         <img src="ANGELA-LOGO-1024x1024.svg" alt="logo da pagina" />
         <div className='header-container'>
@@ -17,6 +19,45 @@ function App() {
         </div>
       </header>
       {/* header end */}
+
+      {/* landing start */}
+      <div className='landing'>
+
+        <section>
+          <div className='org-1'>
+            <div className='org-2'>
+              <img src="multiicons.svg" alt="" />
+              <h3>receita para o seu sucesso</h3>
+            </div>
+            <div>
+              <h1>Tenha o segredo para<br/> o seu sucesso</h1>
+              <p className='body-gray'>Apresento a vocês o curso Maratona do Currículo Ímã,<br/> que vai transformar sua abordagem.</p>
+            </div>
+            <div className='org-2'>
+              <button className='btn-2'> Saber mais </button>
+              <button className='btn-1'> <BsPhone /> Fale conosco </button>
+            </div>
+          </div>
+          <img src="Rectangle.png" alt="imagem do seminario da Angêla" />
+        </section>
+
+        <section>
+          <div className='org-1'>
+
+          </div>
+          <img src="mockup.png" alt="imagem modelo de celular " />
+        </section>
+
+        <section></section>
+        <section></section>
+      </div>
+      {/* landing end */}
+
+      {/* footer start */}
+      <footer>
+
+      </footer>
+      {/* footer end */}
     </>
   )
 }
