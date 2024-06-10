233708bbe0ee71cbc4aa4d940fccf10081ba02f4
Igor Gabriel <igorgabrielr.andrade@gmail.com>
2024-06-10 02:00:25 -0300

    two section finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BsPhone } from "react-icons/bs";
-
+import { FaCheck } from "react-icons/fa6";
 
 function App() {
   return (
@@ -26,26 +26,39 @@ function App() {
         <section>
           <div className='org-1'>
             <div className='org-2'>
-              <img src="multiicons.svg" alt="" />
+              <img src="multiicons.svg" alt="multiplos icons sobrepostos" />
               <h3>receita para o seu sucesso</h3>
             </div>
             <div>
-              <h1>Tenha o segredo para<br/> o seu sucesso</h1>
-              <p className='body-gray'>Apresento a vocês o curso Maratona do Currículo Ímã,<br/> que vai transformar sua abordagem.</p>
+              <h1>Tenha o segredo para<br /> o seu sucesso</h1>
+              <p className='body-gray'>Apresento a vocês o curso Maratona do Currículo Ímã,<br /> que vai transformar sua abordagem.</p>
             </div>
             <div className='org-2 column width'>
               <button className='btn-2 width'> Saber mais </button>
               <button className='btn-1 width'> <BsPhone /> Fale conosco </button>
             </div>
           </div>
-          <img src="Rectangle.png" alt="imagem do seminario da Angêla" />
+          <img className='container-img' src="Rectangle.png" alt="imagem do seminario da Angêla" />
         </section>
 
         <section>
           <div className='org-1'>
-
+            <div>
+              <div className='org-2'>
+                <img src="multiicons.svg" alt="" />
+                <h3>potencialize seu curriculo</h3>
+              </div>
+              <h2>Maratona do currículo ímã</h2>
+              <p className='body-gray'>Apresento a vocês o curso Maratona do Currículo Ímã,<br /> que vai transformar sua abordagem.</p>
+            </div>
+            <div className='bullets'>
+              <p className='body-regular'><span className='circle'><FaCheck /></span>Aumente suas chances de contratação.</p>
+              <p className='body-regular'><span className='circle'><FaCheck /></span>Crie um currículo irresistível para recrutadores.</p>
+              <p className='body-regular'><span className='circle'><FaCheck /></span>Destaque-se no mercado competitivo.</p>
+            </div>
+            <button className='btn-2 width'> Garanta sua vaga <img src="button-asset.svg" alt="icon de melancia" /> </button>
           </div>
-          <img src="" alt="" />
+          <img className='container-img' src="mockup.png" alt="imagem template de celular" />
         </section>
 
         <section></section>
